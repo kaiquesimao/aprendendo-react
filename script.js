f4a6cdb5bc96f1561ba8a89ad98e0ee5713ecf85
kaique <kaique.gabriel.me@gmail.com>
2022-04-19 21:11:50 -0300

    switch case

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-valor = 20
+function pedir(){
+  var valor = prompt("digite um valor de 1 a 4")
 
-for (let a = 0; a < valor; a++){
-  document.write("<br> o valor do a é: " + a)
+  switch (Number(valor)) {
+    case 1:
+      alert("você escolheu 1 = suco")
+      break;
+    
+    case 2:
+      alert("vc escolheu 2")
+      break;
+      
+    case 3:
+      alert("vc escolheu o 3")
+      break;
+
+    case 4:
+      alert("voce escolheu 4")
+      break;
+
+    default:
+      alert("vc não escolheu os numeros certos")
+  }
 }
